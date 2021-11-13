fixing gradient wrt to theta

agent.py
@@ -27,6 +27,8 @@ class Agent:
         s -- threshold (float)
         sigma -- standard deviation of the noise distribution (float)
         """
+        bounds = compute_score_bounds(beta)
+        assert s >= bounds[0] and s <= bounds[1], "cannot compute best response for s out of score bounds"
         try:
             val = newton(
                 Agent._func_derivative_utility(beta, s, self.eta, self.gamma, sigma),
@@ -40,8 +42,8 @@ class Agent:
                     self.eta, self.gamma, beta, s, sigma
                 )
             )
-        val = np.clip(val, a_min=-1.0, a_max=1.0)
-        return val
+        val = np.clip(val, a_min=0., a_max=1.0)
+        return val.reshape(beta.shape)
 
     def plot_best_response_score(self, beta, sigma):
         bounds = compute_score_bounds(beta)
@@ -89,14 +91,16 @@ class Agent:
     def br_score_function_beta(self, s, sigma):
         thetas = np.linspace(-np.pi, np.pi, 100)
         br = []
+        valid_theta = []
         for theta in thetas:
             beta = np.array([np.cos(theta), np.sin(theta)]).reshape(2, 1)
             bounds = compute_score_bounds(beta)
-            s_clip = np.clip(s, bounds[0], bounds[1])
-            br.append(np.matmul(beta.T, self.best_response(beta, s_clip, sigma)).item())
+            if s >= bounds[0] and s <= bounds[1]:
+                br.append(np.matmul(beta.T, self.best_response(beta, s, sigma)).item())
+                valid_theta.append(theta)
 
-        f = interp1d(thetas, br)
-        return f
+        f = interp1d(valid_theta, br)
+        return f, valid_theta
 
     def br_gradient_beta(self, beta, s, sigma):
         """
@@ -111,14 +115,7 @@ class Agent:
         best_response -- (D, 1) array
         jacobian -- (D, D) matrix
         """
-        G = np.diag(self.gamma.flatten())
-        best_response = self.best_response(beta, s, sigma)
-        arg = s - np.matmul(beta.T, best_response)
-        prob = norm.pdf(arg, loc=0, scale=sigma)
-        prob_prime = -(arg / (sigma ** 2)) * norm.pdf(arg, loc=0, scale=sigma)
-        rank_one_mat = np.matmul(beta, beta.T)
-        jacobian = np.transpose(prob * np.linalg.inv(2 * G + prob_prime * rank_one_mat))
-        return best_response, jacobian
+        pass
 
     def br_gradient_s(self, beta, s, sigma):
         """
@@ -133,7 +130,8 @@ class Agent:
         best_response -- (D, 1) array
         deriv_s -- (D, 1) array
         """
-
+        bounds = compute_score_bounds(beta)
+        assert s >= bounds[0] and s <= bounds[1]
         G = np.diag(self.gamma.flatten())
         best_response = self.best_response(beta, s, sigma)
         arg = s - np.matmul(beta.T, best_response)
@@ -144,6 +142,22 @@ class Agent:
         )
         return best_response, deriv_s
 
+    def br_gradient_theta(self, theta, s, sigma):
+
+        beta = np.array([np.cos(theta), np.sin(theta)]).reshape(2, 1)
+        bounds = compute_score_bounds(beta)
+        assert s >= bounds[0] and s <= bounds[1]
+        best_response = self.best_response(beta, s, sigma)
+        arg = s - np.matmul(beta.T, best_response)
+        prob = norm.pdf(arg, loc=0, scale=sigma)
+        prob_prime = -(arg / (sigma ** 2)) * norm.pdf(arg, loc=0, scale=sigma)
+        rank_one_mat = np.matmul(beta, beta.T)
+        dbeta_dtheta = np.array([-np.sin(theta), np.cos(theta)]).reshape(2, 1)
+        G = np.diag(self.gamma.flatten())
+        first = 2 * G + prob_prime * rank_one_mat
+        inv_mat = np.linalg.inv(first)
+        second = - (prob_prime * np.matmul(best_response.T, dbeta_dtheta)).item() * beta + prob_prime * dbeta_dtheta
+        return best_response, np.matmul(inv_mat, second)
 
 if __name__ == "__main__":
     eta = np.array([0.5, 0.5]).reshape(2, 1)

agent_distribution.py
@@ -2,6 +2,7 @@ from scipy.stats import norm
 from scipy.interpolate import interp1d
 import matplotlib.pyplot as plt
 import numpy as np
+import tqdm
 
 from utils import compute_score_bounds
 from agent import Agent
@@ -28,7 +29,7 @@ class AgentDistribution:
         if types is None:
             # Generate n_types agent types randomly
             etas = np.random.uniform(0.4, 0.6, size=n_types * d).reshape(n_types, d, 1)
-            gammas = np.ones((n_types, d, 1)) * 4
+            gammas = np.ones((n_types, d, 1)) * 8
         #            gammas = np.random.uniform(1.0, 2.0, size=n_types * d).reshape(
         else:
             etas = types["etas"]
@@ -205,10 +206,60 @@ class AgentDistribution:
             self.quantile_best_response(beta, s, sigma, q) for s in thresholds
         ]
 
+
         plt.plot(thresholds, quantile_br)
         plt.xlabel("Thresholds")
         plt.ylabel("Quantile BR")
         plt.title("Quantile BR vs. Threshold")
+        
+    def quantile_mapping_vary_s(self, beta, sigma, q):
+        """This method returns the quantile mapping function q(beta, s). 
+        
+        Keyword arguments:
+        beta -- model parameters (Nx1)
+        sigma -- standard deviation of noise distribution(float)
+        q -- quantile between 0 and 1 (float)
+        """
+        bounds = compute_score_bounds(beta)
+        thresholds = np.linspace(bounds[0], bounds[1], 50)
+        quantile_map = []
+        
+        for s in tqdm.tqdm(thresholds):
+            cdf_vals = []
+            for r in thresholds:
+                cdf_vals.append(self.best_response_cdf(beta, s, sigma, r))
+            inverse_cdf_s = interp1d(cdf_vals, thresholds, kind="linear") 
+            quantile_map.append(inverse_cdf_s(q))
+        q = interp1d(thresholds, quantile_map)
+        return q
+    
+    def quantile_mapping_vary_beta(self, s, sigma, q):
+        """This method returns the quantile mapping function q(beta, s). 
+        
+        Keyword arguments:
+        s -- threshold (float)
+        sigma -- standard deviation of noise distribution(float)
+        q -- quantile between 0 and 1 (float)
+        """
+        thetas = np.linspace(-np.pi, np.pi, 50)
+        quantile_map = []
+        valid_theta = []
+        for theta in tqdm.tqdm(thetas):
+            cdf_vals = []
+            beta = np.array([np.cos(theta), np.sin(theta)]).reshape(2, 1)
+            bounds = compute_score_bounds(beta)
+            score_range = np.linspace(bounds[0], bounds[1], 50)
+            if s >= bounds[0] and s <= bounds[1]:
+                for r in score_range:
+                    cdf_vals.append(self.best_response_cdf(beta, s, sigma, r))
+                inverse_cdf_theta = interp1d(cdf_vals, score_range, kind="linear")
+                plt.plot(cdf_vals, score_range)
+                plt.xlabel("q")
+                plt.ylabel("F^-1(q)")
+                quantile_map.append(inverse_cdf_theta(q))
+                valid_theta.append(theta)
+        q = interp1d(valid_theta, quantile_map, kind="linear")
+        return q, valid_theta
 
     def quantile_fixed_point_true_distribution(self, beta, sigma, q, plot=False):
         bounds = compute_score_bounds(beta)
@@ -244,18 +295,44 @@ class AgentDistribution:
 
         return f(q)
 
-    def best_response_pdf(self, beta, s, sigma):
+    def best_response_pdf(self, beta, s, sigma, r):
+        bounds = compute_score_bounds(beta)
+        if s < bounds[0]:
+            return 0.
+        if s > bounds[1]:
+            return 0.
+
         pdf_val = 0.0
+
         for i, agent in enumerate(self.agents):
             pdf_val += (
                 norm.pdf(
-                    s - np.matmul(beta.T, agent.best_response(beta, s, sigma)),
+                    r - np.matmul(beta.T, agent.best_response(beta, s, sigma)),
                     loc=0.0,
                     scale=sigma,
                 )
                 * self.prop[i]
             )
         return pdf_val.item()
+
+    def best_response_cdf(self, beta, s, sigma, r):
+        bounds = compute_score_bounds(beta)
+        if s < bounds[0]:
+            return 0.
+        if s > bounds[1]:
+            return 1.
+
+        cdf_val = 0.0
+        for i, agent in enumerate(self.agents):
+             cdf_val += (
+                norm.cdf(
+                    r - np.matmul(beta.T, agent.best_response(beta, s, sigma)),
+                    loc=0.0,
+                    scale=sigma,
+                )
+                * self.prop[i]
+            )
+        return cdf_val.item()
 
     def quantile_fixed_point_naive(self, beta, sigma, q, plot=False):
         bounds = compute_score_bounds(beta)

reparametrized_gradient.py
@@ -5,6 +5,70 @@ import tqdm
 
 from utils import compute_score_bounds
 
+def expected_gradient_pi_theta(agent_dist, theta, sigma, r, f):
+    dim = agent_dist.d
+    assert dim==2, "Method does not work for dimension {}".format(dim)
+
+    beta = np.array([np.cos(theta), np.sin(theta)]).reshape(2, 1)
+    s = f(theta)
+    
+    br_dist, jacobian_dist = agent_dist.br_gradient_beta_distribution(beta, s, sigma)
+
+    z = r - np.array([np.matmul(beta.T, x) for x in  br_dist]).reshape(len(br_dist), 1)
+    prob = norm.pdf(z, loc=0., scale=sigma)
+    vec = np.array([np.matmul(beta.T, jacobian_dist[i]) + br_dist[i].T for i in range(len(br_dist))]).reshape(len(agent_dist.agents), len(beta))
+    dbeta_dtheta = np.array([-np.sin(theta), np.cos(theta)]).reshape(2, 1)
+    vec_chain = np.array([np.matmul(x.T, dbeta_dtheta).item() for x in vec]).reshape(2, 1)
+    res = -prob * vec_chain * agent_dist.prop.reshape(len(agent_dist.prop), 1)
+    grad_pi_theta = np.sum(res, axis=0)
+
+    return grad_pi_theta
+
+def empirical_gradient_pi_theta(agent_dist, beta, s, sigma, r, perturbation_size=0.1):
+    """Method that returns the empirical gradient of pi wrt to beta incurred given an agent distribution and model and threshold.
+    Assumes that there is an model true_beta when applied to the agents' hidden eta features
+    optimally selects the top agents.
+
+    Keyword args:
+    agent_dist -- AgentDistribution
+    beta -- model parameters (N, 1) array
+    s -- threshold (float)
+    sigma -- standard deviation of the noise (float)
+
+    Returns:
+    gamma_pi_beta - gradient of pi at beta
+    """
+    perturbations = (2 * bernoulli.rvs(p=0.5, size=agent_dist.n * (agent_dist.d-1)).reshape(agent_dist.n, agent_dist.d-1, 1) -1 ) * perturbation_size
+    scores = []
+    
+    bounds = compute_score_bounds(beta)
+     
+    interpolators = []
+    for agent in agent_dist.agents:
+        f = agent.br_score_function_beta(s, sigma)
+        interpolators.append(f)
+
+    for i in range(agent_dist.n):
+        theta_perturbed = theta + perturbations[i]
+        if theta_perturbed < -np.pi:
+            theta_perturbed += 2 * np.pi
+        if theta_perturbed > np.pi:
+            theta_perturbed -= 2 * np.pi
+        agent_type = agent_dist.n_agent_types[i]
+        br_score = interpolators[agent_type](theta_perturbed)
+        scores.append(br_score.item())
+
+    scores = np.array(scores).reshape(agent_dist.n, 1)
+    noise = norm.rvs(loc=0., scale=sigma, size=agent_dist.n).reshape(agent_dist.n, 1)
+    noisy_scores = np.clip(scores + noise, a_min=bounds[0], a_max=bounds[1])
+    indicators = noisy_scores <= r
+    
+    perturbations = perturbations.reshape(agent_dist.n, agent_dist.d-1)
+    Q = np.matmul(perturbations.T, perturbations)
+    gamma_pi_theta = np.linalg.solve(Q, np.matmul(perturbations.T, indicators))
+    return gamma_pi_theta
+
+
 def expected_gradient_loss_theta(agent_dist, theta, sigma, f, true_beta=None):
     """Method computes partial L(theta)/partial theta.
 
@@ -33,9 +97,9 @@ def expected_gradient_loss_theta(agent_dist, theta, sigma, f, true_beta=None):
     br_dist, jacobian_dist = agent_dist.br_gradient_beta_distribution(beta, s, sigma)
     z = s - np.array([np.matmul(beta.T, x) for x in  br_dist]).reshape(len(br_dist), 1)
     prob = norm.pdf(z, loc=0., scale=sigma)
-    dg_dtheta = np.array([-np.sin(theta), np.cos(theta)]).reshape(2, 1)
+    dbeta_dtheta = np.array([-np.sin(theta), np.cos(theta)]).reshape(2, 1)
     vec = np.array([np.matmul(beta.T, jacobian_dist[i]) + br_dist[i].T for i in range(len(br_dist))]).reshape(len(agent_dist.agents), 1, len(beta))
-    final_scalar = np.array([np.matmul(v, dg_dtheta ).item() for v in vec]).reshape(len(agent_dist.agents), 1)
+    final_scalar = np.array([np.matmul(v, dbeta_dtheta ).item() for v in vec]).reshape(len(agent_dist.agents), 1)
     res = prob * final_scalar * true_scores * agent_dist.prop.reshape(len(agent_dist.prop), 1)
     d_l_d_theta = np.sum(res, axis=0)
     return d_l_d_theta