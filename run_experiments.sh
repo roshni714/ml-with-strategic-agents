1a7dcba2399ec99e7f77154f3993a3a5a9ff35fd
roshni714 <roshni714@gmail.com>
2021-12-28 17:36:43 -0800

    experiments w beta

--- run_experiments.sh
+++ run_experiments.sh
@@ -12,7 +12,9 @@ MAX_ITER=100
 
 for SEED in "${seeds[@]}";
 do
-    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save dim_10_total --seed $SEED --gradient_type "total_deriv"
+    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save dim_10 --seed $SEED --gradient_type "total_deriv"
+    python train_beta.py main --n 1000000 --d 10 --perturbation_s 0.2 --perturbation_beta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 10 --save dim_10 --seed $SEED --gradient_type "partial_deriv_loss_beta"
+
 #    python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 20 --save challenge_n_types_20_n_1000000 --seed $SEED
 #    python train.py main --n 1000000 --perturbation_s 0.2 --perturbation_theta 0.025 --learning_rate 1. --max_iter $MAX_ITER --n_types 50 --save challenge_n_types_50_n_1000000 --seed $SEED
 done
